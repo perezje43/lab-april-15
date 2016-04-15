83a4adb15a0aebca49da32b8abebb3a95fb853a2
Jeremy Perez <perezje43@gmail.com>
2016-04-15 13:41:19 -0700

    Finished problem 2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 function sum(numberOne, numberTwo) {
   console.log('\"The sum of ' + numberOne + ' and ' + numberTwo + ' is ' + (numberOne + numberTwo) + '.\"');
 }
+
+function multiply(numberOne, numberTwo) {
+  console.log('\"The product of ' + numberOne + ' and ' + numberTwo + ' is ' + (numberOne * numberTwo) + '.\"');
+}
